652189b67b6d6a2e3b236746f04d05af722b68a1
FRC2495 <frc2495@gmail.com>
2025-03-03 17:44:06 -0500

    drive train calibration

--- src/main/java/frc/robot/subsystems/SwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/SwerveDrivetrain.java
@@ -53,11 +53,11 @@ public class SwerveDrivetrain extends SubsystemBase {
 
 	// offset needs to be subtracted from the virtual position
 
-	public static final double FRONT_LEFT_VIRTUAL_OFFSET_RADIANS = 3.073;//-3.0345;//-2.369; // adjust as needed so that virtual (turn) position of wheel is zero when straight
-	public static final double REAR_LEFT_VIRTUAL_OFFSET_RADIANS = -1.044;//-.970;//-0.415;//-1.220; // adjust as needed so that virtual (turn) position of wheel is zero when straight
+	public static final double FRONT_LEFT_VIRTUAL_OFFSET_RADIANS = 3.109;//2.500;//3.073; // adjust as needed so that virtual (turn) position of wheel is zero when straight
+	public static final double REAR_LEFT_VIRTUAL_OFFSET_RADIANS = -0.965;//-2.479;//-1.044; // adjust as needed so that virtual (turn) position of wheel is zero when straight
 	
-	public static final double FRONT_RIGHT_VIRTUAL_OFFSET_RADIANS = -.709;//-0.422;//-2.339; //-2.827 // adjust as needed so that virtual (turn) position of wheel is zero when straight
-	public static final double REAR_RIGHT_VIRTUAL_OFFSET_RADIANS = -2.74;//-2.739;//0.5431; // adjust as needed so that virtual (turn) position of wheel is zero when straight
+	public static final double FRONT_RIGHT_VIRTUAL_OFFSET_RADIANS = -0.552;//0.981;//-.709; // adjust as needed so that virtual (turn) position of wheel is zero when straight
+	public static final double REAR_RIGHT_VIRTUAL_OFFSET_RADIANS = -2.743;//-2.743;//-2.74; // adjust as needed so that virtual (turn) position of wheel is zero when straight
 
 	public static final int GYRO_ORIENTATION = -1; // might be able to merge with kGyroReversed
 
