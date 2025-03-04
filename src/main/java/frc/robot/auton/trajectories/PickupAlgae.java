98c4833fcd0ad9323e0f928b4bfb8cda17433b22
FRC2495 <frc2495@gmail.com>
2025-03-03 20:13:39 -0500

    Made a few parallel commands

---
+++ src/main/java/frc/robot/auton/trajectories/PickupAlgae.java
@@ -0,0 +1,34 @@
+package frc.robot.auton.common;
+
+import java.util.List;
+
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+
+//import edu.wpi.first.math.geometry.Pose2d;
+//import edu.wpi.first.math.geometry.Rotation2d;
+//import edu.wpi.first.math.trajectory.Trajectory;
+//import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+
+import frc.robot.auton.AutonConstants;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.*;
+import frc.robot.commands.drivetrain.*;
+import frc.robot.commands.algae_roller.*;
+import frc.robot.commands.slider.*;
+import frc.robot.commands.neck.*;
+
+
+public class PickupAlgae extends ParallelCommandGroup{
+	
+	public PickupAlgae(AlgaeRoller algae_roller, Slider slider, Neck neck) {
+
+		addCommands(
+
+			new NeckMoveDownWithStallDetection(neck),
+
+			new SliderExtendWithStallDetection(slider),
+			
+			new algaeRollerTimedRoll(algae_roller, .5)
+		);
+	}
+}
