1e7268d82dd1c32d46c9cb1c5e06066b1b0db9e6
FRC2495 <frc2495@gmail.com>
2025-03-16 09:02:15 -0400

    fixed neckmovetocoralreef finish condition

--- src/main/java/frc/robot/commands/neck/NeckMoveToCoralReefWithStallDetection.java
+++ src/main/java/frc/robot/commands/neck/NeckMoveToCoralReefWithStallDetection.java
@@ -33,9 +33,9 @@ public class NeckMoveToCoralReefWithStallDetection extends Command {
 	// Make this return true when this Command no longer needs to run execute()
 	@Override
 	public boolean isFinished() {
-		//return !neck.tripleCheckMove() || neck.tripleCheckIfStalled();
+		return neck.tripleCheckIfStalled();
 		//return neck.tripleCheckIfStalled();
-		return false;
+		//return false;
 	}
 
 	// Called once after isFinished returns true
