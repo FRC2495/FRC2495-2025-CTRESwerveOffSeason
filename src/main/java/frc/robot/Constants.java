3d7808551536c26a0dcc933c310dae27a776d6b4
FRC2495 <frc2495@gmail.com>
2025-03-25 18:23:34 -0400

    auton pathplanner p values

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -146,8 +146,8 @@ public final class Constants {
 		public static final double Y_CONTROLLER_P = 1;
 		public static final double THETA_CONTROLLER_P = 1;
 
-		public static final double TRANSLATION_HOLONOMIC_CONTROLLER_P = 3.35;//3.1; //2.95;//2.8;//2.9; //.5; //2.75; //4.0;
-		public static final double ROTATION_HOLONOMIC_CONTROLLER_P = 1.7;//1.8;//2;//1; //2; //5; //2.5
+		public static final double TRANSLATION_HOLONOMIC_CONTROLLER_P = 1.7;//3.1; //2.95;//2.8;//2.9; //.5; //2.75; //4.0;
+		public static final double ROTATION_HOLONOMIC_CONTROLLER_P = 2;//1;//1.8;//2;//1; //2; //5; //2.5
 
 		public static final double TRANSLATION_HOLONOMIC_CONTROLLER_I = 0;
 		public static final double ROTATION_HOLONOMIC_CONTROLLER_I = 0;
@@ -173,9 +173,9 @@ public final class Constants {
 		public static final double Y_ALIGNMENT_TOLERANCE = .3;//.05;
 		public static final double ROT_ALIGNMENT_TOLERANCE = .3;//.05;
 
-		public static final double X_REEF_ALIGNMENT_P = 0.1;//1;//3.3;
-		public static final double Y_REEF_ALIGNMENT_P = 0.1;//1;//3.3;
-		public static final double ROT_REEF_ALIGNMENT_P = 0.1;//1;//0.058;
+		public static final double X_REEF_ALIGNMENT_P = 0.4;//1;//3.3;
+		public static final double Y_REEF_ALIGNMENT_P = 0.4;//1;//3.3;
+		public static final double ROT_REEF_ALIGNMENT_P = 0.4;//1;//0.058;
 
 		public static final double DONT_SEE_TAG_WAIT_TIME = 0.3;
 		public static final double POSE_VALIDATION_TIME = 0.3;
