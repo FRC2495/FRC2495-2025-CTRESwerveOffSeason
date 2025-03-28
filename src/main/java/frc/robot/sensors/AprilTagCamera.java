fe221039a416c407230b417d2e402d371700771e
ginaaaw <25giwong@gmail.com>
2025-03-28 01:31:55 -0400

    javadocs in apriltagcamera

--- src/main/java/frc/robot/sensors/AprilTagCamera.java
+++ src/main/java/frc/robot/sensors/AprilTagCamera.java
@@ -9,28 +9,17 @@ import java.util.List;
 import java.util.Optional;
 
 import org.photonvision.EstimatedRobotPose;
-//import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
 import org.photonvision.PhotonPoseEstimator.PoseStrategy;
-//import org.photonvision.PhotonPoseEstimator;
 import org.photonvision.PhotonUtils;
-//import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
-//import edu.wpi.first.apriltag.AprilTagFieldLayout;
-//import edu.wpi.first.apriltag.AprilTagFields;
-//import edu.wpi.first.math.geometry.Pose2d;
-//import edu.wpi.first.math.geometry.Pose3d;
-//import edu.wpi.first.math.geometry.Rotation3d;
-//import edu.wpi.first.math.geometry.Transform3d;
-//import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
-//import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import frc.robot.Constants.AprilTags;
 import frc.robot.RobotContainer;
 import frc.robot.interfaces.ICamera;
@@ -56,17 +45,25 @@ public class AprilTagCamera extends PhotonCamera implements ICamera {
 		new Rotation3d(CAMERA_ROLL_RADIANS, CAMERA_PITCH_RADIANS, CAMERA_YAW_RADIANS)
 	);
 
-	//static final double APRILTAG_CAMERA_SHOOTER_ALIGNMENT_CORRECTION_DEGREES = 5.0; // apply offset in degrees to compensate for shooter being a bit crooked - TODO adjust as needed
-	//static final AprilTagFieldLayout FIELD_LAYOUT = AprilTagFieldLayout.loadField(AprilTagFields.k2025ReefscapeWelded);
-
+	/**
+     * Constructs a PhotonCamera using inherited superclass.
+     * Instantiates the DEFAULT_CAM_NAME variable as the name of the camera. 
+	 * This will allow the robot to connect the camera from the PhotonVision UI to this class.
+	 * Instantiates the estimator as a PhotonPoseEstimator using the current year's field layout, 
+	 * pose estimate setting and camera position on robot.
+     */
 	public AprilTagCamera() {
 		super(DEFAULT_CAM_NAME);
 		estimator = new PhotonPoseEstimator(RobotContainer.FIELD_LAYOUT, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, robotToCam);
 	}
 
+	/**
+     * Updates the list of pipeline results sent by PhotonVision since the last call to getAllUnreadResults().
+     * Stores all of the results into a List object to be called in other methods in this class.
+	 * Called periodically in Robot.java.
+     */
 	public void updateCacheResults(){ // we call this periodically in robot.java 
 		cachedResults = getAllUnreadResults(); // note: calling this function clears the internal FIFO queue. We call this exactly ONCE per loop.
-		//System.out.println(cachedResults);
 		//var result = this.getLatestResult();
 		/*if(result!=null && result.hasTargets()) {
 			latestID = result.getBestTarget().getFiducialId();
@@ -74,6 +71,14 @@ public class AprilTagCamera extends PhotonCamera implements ICamera {
 			latestID = -1;}
 	*/}
 
+	/**
+     * Returns the estimated robot pose based on 3D tracking of AprilTags.
+	 * Looks through the list of pipeline results sent by PhotonVision.
+	 * Finds changes in the results that differ from current belief of position.
+	 * Updates estimated robot pose based on changes of the AprilTags the robot sees.
+     *
+     * @return The estimated robot pose based on 3D tracking of ApriLTags
+     */
 	public Optional<EstimatedRobotPose> getGlobalPose(){
 		Optional<EstimatedRobotPose> globalPose = Optional.empty();
 		for (var change : cachedResults) {
@@ -82,6 +87,13 @@ public class AprilTagCamera extends PhotonCamera implements ICamera {
 		return globalPose;
 	}
 
+	/**
+     * Returns the AprilTag target-to-camera transformation.
+	 * Checks for targets in camera vision.
+	 * If target is seen, returns the latest AprilTag target and sends (X, Y, Z, rotation coordinates), as shown in the UI.
+     *
+     * @return The AprilTag target-to-camera transformation
+     */
 	public Transform3d getBestCameraToTargetTransform(){
 		Transform3d bestCameraToTarget = null;
 		//var results = cachedResults;
@@ -104,6 +116,14 @@ public class AprilTagCamera extends PhotonCamera implements ICamera {
 		return bestCameraToTarget;
 	}
 
+	/**
+     * Returns the X coordinate from the latest target-to-camera transformation information.
+	 * Checks for valid transformation information.
+	 * Transforms the current AprilTag target and sends X coordinate, as seen in the UI.
+     *
+	 * @param currentPose The latest target-to-camera transformation information.
+     * @return The X coordinate from the latest target-to-camera transformation information if found. If not, returns 0.0.
+     */
 	public double getBestCameraToTargetX(Transform3d currentPose) {
 		if (currentPose != null) {
 			return currentPose.getX(); 
@@ -112,6 +132,14 @@ public class AprilTagCamera extends PhotonCamera implements ICamera {
 		return 0.0; 
 	}
 
+	/**
+     * Returns the Y coordinate from the latest target-to-camera transformation information.
+	 * Checks for valid transformation information.
+	 * Transforms the current AprilTag target and sends Y coordinate, as seen in the UI.
+     *
+	 * @param currentPose The latest target-to-camera transformation information.
+     * @return The Y coordinate from the latest target-to-camera transformation information if found. If not, returns 0.0.
+     */
 	public double getBestCameraToTargetY(Transform3d currentPose) {
 		if (currentPose != null) {
 			return currentPose.getY(); 
@@ -120,6 +148,14 @@ public class AprilTagCamera extends PhotonCamera implements ICamera {
 		return 0.0; 
 	}
 
+	/**
+     * Returns the rotation of the robot based on  the latest target-to-camera transformation information.
+	 * Checks for valid transformation information.
+	 * Transforms the current AprilTag target and sends rotation data, as seen in the UI.
+     *
+	 * @param currentPose The latest target-to-camera transformation information.
+     * @return The rotation of the robot compared to the AprilTag from the recieved target-to-camera transformation information in radians if found. If not, returns pi.
+     */
 	public double getBestCameraToTargetRotationRadians(Transform3d currentPose) {
 		if (currentPose != null) {
 			return currentPose.getRotation().getAngle();
@@ -128,10 +164,17 @@ public class AprilTagCamera extends PhotonCamera implements ICamera {
 		return Math.PI; 
 	}
 
+	/**
+     * Returns whether the AprilTag is in sight of the camera.
+	 * Checks for target in camera view.
+	 * Sends true if target is an AprilTag on the reef.
+     *
+     * @return Whether the AprilTag is in sight of the camera
+     */
 	public boolean isTargetVisible()
 	{
 		boolean targetVisible = false;
-		double targetYaw = 0.0;
+		//double targetYaw = 0.0;
 		//var results = cachedResults;
 		if (!cachedResults.isEmpty()) {
 			// Camera processed a new frame since last
@@ -152,8 +195,8 @@ public class AprilTagCamera extends PhotonCamera implements ICamera {
 					|| target.getFiducialId() == AprilTags.BLUE_REEF_SIDE_D 
 					|| target.getFiducialId() == AprilTags.BLUE_REEF_SIDE_E 
 					|| target.getFiducialId() == AprilTags.BLUE_REEF_SIDE_F) {
-						// Found Tag 7, record its information
-						targetYaw = target.getYaw();
+						// Found High Value Target, record its information
+						//targetYaw = target.getYaw();
 						targetVisible = true;
 					}
 				}
@@ -162,11 +205,23 @@ public class AprilTagCamera extends PhotonCamera implements ICamera {
 		return targetVisible;
 	}
 	
+	/**
+     * Returns distance in meters from camera to target.
+	 * Calls getDistanceToHighValueTarget() method.
+     *
+     * @return The distance in meters from camera to target.
+     */
 	public double getDistanceToTarget() {
 		//return getDistanceToBestTarget();
 		return getDistanceToHighValueTarget();
 	}
 
+	/**
+     * Returns the angle in radians from camera to middle of target.
+	 * Calls getAngleToTurnToHighValueTarget() method.
+     *
+     * @return The angle in radians from camera to middle of target.
+     */
 	public double getAngleToTurnToTarget() {
 		//double angle = getAngleToTurnToBestTarget();
 		double angle = getAngleToTurnToHighValueTarget();
@@ -179,6 +234,13 @@ public class AprilTagCamera extends PhotonCamera implements ICamera {
 		return angle;
 	}
 
+	/**
+     * Returns the distance from the AprilTag to the camera in inches only if the target is considered a best target.
+	 * Checks if the latest AprilTag target recieved is the best target in list before retrieving.
+     *
+	 * @throws x If no AprilTag target is found or target found is not considered a best target
+     * @return The distance from the AprilTag to the camera in inches only if the target is considered a best target.
+     */
 
 	public double getDistanceToBestTarget() {
 		try
@@ -201,6 +263,13 @@ public class AprilTagCamera extends PhotonCamera implements ICamera {
 		}
 	}
 
+	/**
+     * Returns yaw of the target compared to the camera if the target is considered a best target.
+	 * Checks if the latest AprilTag target recieved is the best target in list before retrievin.
+     *
+	 * @throws x If no AprilTag target was found or was not the best target if found.
+     * @return The yaw of the target compared to the camera if the target is considered a best target.
+     */
 	public double getYaw() {
 		try
 		{
@@ -218,6 +287,12 @@ public class AprilTagCamera extends PhotonCamera implements ICamera {
 		}	
 	}
 
+	/**
+     * Returns yaw of the target compared to the camera if the target is considered a best target.
+	 * Calls getYaw() method and returns as positive.
+     *
+     * @return The positive yaw of the target compared to the camera if the target is considered a best target.
+     */
 	public double getAngleToTurnToBestTarget()
 	{
 		return +getYaw();
@@ -227,6 +302,13 @@ public class AprilTagCamera extends PhotonCamera implements ICamera {
 		return latestID;
 	}*/
 
+	/**
+     * Returns pitch of the target compared to the camera if the target is a best target.
+	 * Checks if the latest AprilTag target recieved is the best target in list before retrieving.
+	 * 
+	 * @throws x If no AprilTag target found or target found is not considered a best target.
+     * @return The pitch of the target compared to the camera if the target is considered a best target.
+     */
 	public double getPitch() {
 		try
 		{
@@ -245,6 +327,13 @@ public class AprilTagCamera extends PhotonCamera implements ICamera {
 	
 	}
 
+	/**
+     * Returns skew of the target compared to the camera if the target is a best target.
+	 * Checks if the latest AprilTag target recieved is the best target in list before retrieving.
+	 * 
+	 * @throws x If no AprilTag target found or target found is not considered a best target.
+     * @return The skew of the target compared to the camera if the target is considered a best target.
+     */
 	public double getSkew() {
 		try
 		{
@@ -262,6 +351,13 @@ public class AprilTagCamera extends PhotonCamera implements ICamera {
 		}	
 	}
 
+	/**
+     * Returns AprilTag ID in camera vision if the target is a best target.
+	 * Checks if the latest AprilTag target recieved is the best target in list before retrieving.
+	 * 
+	 * @throws x If no AprilTag target found or target found is not considered a best target.
+     * @return The AprilTag ID in view of camera if it is considered a best target.
+     */
 	public int getAprilTagId() {
 		try
 		{
@@ -278,7 +374,14 @@ public class AprilTagCamera extends PhotonCamera implements ICamera {
 		}
 	}
 
-
+	/**
+     * Returns AprilTag ID in camera vision if the target is a high value target.
+	 * Checks if the latest AprilTag target recieved is a high value target.
+	 * In this case, a high value target is a target on the reef.
+	 * 
+	 * @param result The latest result from the camera pipeline.
+     * @return The AprilTag ID in view of camera if it is considered a best target.
+     */
 	public PhotonTrackedTarget getHighValueTarget(PhotonPipelineResult result) {
 
 		if (result.hasTargets()) {
@@ -321,6 +424,13 @@ public class AprilTagCamera extends PhotonCamera implements ICamera {
 		return null; // no high value target found
 	}
 
+	/**
+     * Returns distance from AprilTag target to camera in inches if the target is a high value target.
+	 * Checks if the latest AprilTag target recieved is a high value target before retrieving.
+	 * 
+	 * @throws x If no AprilTag target found or target found is not a high value target
+     * @return The AprilTag ID in view of camera if it is considered a best target.
+     */
 	public double getDistanceToHighValueTarget() {
 		try
 		{
@@ -342,6 +452,13 @@ public class AprilTagCamera extends PhotonCamera implements ICamera {
 		}
 	}
 
+	/**
+     * Returns angle between the center of the high value target and the camera in radians.
+	 * Checks if the latest AprilTag target is a high value target before recieving.
+	 * 
+	 * @throws x If the AprilTag target is a high value target and is valid.
+     * @return The angle between the center of the high value target and the camera in radians.
+     */
 	public double getAngleToTurnToHighValueTarget()
 	{
 		try
